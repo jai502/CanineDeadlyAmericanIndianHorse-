35ef9e201fbf2351272179532c0e8675bef02542
Peter <Peter@DIONYSUS.home>
2016-02-25 16:49:06 +0000

    Commiting encryption classes

--- Encrypt.java
+++ Encrypt.java
@@ -11,8 +11,8 @@ import org.apache.commons.codec.binary.Base64;
 *	Date of last update:		25/02/16
 *	Version Number:				1
 *
-*	Commit date:
-*	Description:
+*	Commit date:	25/02/16
+*	Description: 	Encryption classes for password hashing etc.
 */
 public class Encrypt
 {
@@ -23,6 +23,7 @@ public class Encrypt
 	
 	public Encrypt()
 	{
+		// TEST THE HASHING CLASSES
 		String hashedPassword = null;
 		boolean passwordCheck = false;
 		
@@ -30,7 +31,6 @@ public class Encrypt
 		try {
 			hashedPassword= getSaltedHash("testpassword");
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		System.out.println(hashedPassword);
@@ -39,42 +39,51 @@ public class Encrypt
 		try {
 			passwordCheck = check("testpassword", hashedPassword);
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		System.out.println("Password check result " + passwordCheck);
+		System.out.println("Password check result: " + passwordCheck);
 		
 	}
 	
 	
-	// Hash the password
+	// Hash password with salt as key
 	public static String hash(String password, byte[] salt) throws Exception
-	{
+	{	
+		// Check the entered password is valid
 		if (password == null || password.length() == 0)
 		{
-			throw new IllegalArgumentException("Empty passwords are not supported.");
+			throw new IllegalArgumentException("Empty passwords are not allowed");
 		}
+		// Initialise key generator with desired encryption
 		SecretKeyFactory f = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+		// Generate key and hash password
 		SecretKey key = f.generateSecret(new PBEKeySpec(password.toCharArray(), salt, iterations, desiredKeyLen));
+		// Return hashed password
 		return Base64.encodeBase64String(key.getEncoded());
 	}
 	
-	// Combine salt with Hash
+	// Combine hashed password with salt key
 	public static String getSaltedHash(String password) throws Exception
-	{
+	{	
+		// Generate salt 
 		byte[] salt = SecureRandom.getInstance("SHA1PRNG").generateSeed(saltLen);
 		// Append salt to password
 		return Base64.encodeBase64String(salt) + "$" + hash(password, salt);
 	}
 	
+	// Check stored hash against entered password
 	public static boolean check(String password, String stored) throws Exception
 	{
+		// Separate salt and hashed password
 		String[] saltAndPass = stored.split("\\$");
+		// Check that saltAndPass array only contains two elements
 		if (saltAndPass.length != 2)
 		{
 			throw new IllegalStateException("The stored password should have the form 'salt$hash'");	
 		}
+		// Hash user entered password based on stored salt
 		String hashOfInput = hash(password, Base64.decodeBase64(saltAndPass[0]));
+		// Check hashed entered password against stored password and return result
 		return hashOfInput.equals(saltAndPass[1]);
 	}
 	
