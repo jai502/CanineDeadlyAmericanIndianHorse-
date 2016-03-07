a47ed2905ecb6efeac5c0b8ac6970c4a5732ae6f
danieldreyfus <danieldreyfus@nas10-240-176-218.york.ac.uk>
2016-03-07 15:24:05 +0000

    Audio Handler with a test class

---
+++ Sound.java
@@ -0,0 +1,5 @@
+import sun.audio.*;
+
+public class Sound {
+
+}
---
+++ audioClasses/testAudio.java
@@ -0,0 +1,36 @@
+package audioClasses;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class testAudio extends Application {
+
+	public static void main (String[] args) {
+		
+		launch(args);
+		
+	}
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+		primaryStage.setTitle("Audio");
+		
+		BorderPane bp = new BorderPane();
+		
+		Audio audio = new Audio("audioClasses/Chum Drum.mp3");
+
+		
+		bp.setBottom(audio);
+		
+		Scene scene = new Scene(bp, 800, 600);
+		
+		primaryStage.setScene(scene);
+		primaryStage.sizeToScene();
+		primaryStage.show();
+	}
+		
+}
