dacfe28b2369f955001dde64b1dc9f8937406fb0
ac1362 <ac1362@ELCLAS401.its.york.ac.uk>
2016-04-25 13:11:11 +0100

    Added server console interaction

--- server/ClientRequestHandler.java
+++ server/ClientRequestHandler.java
@@ -4,6 +4,7 @@ package server;
 //import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 //import java.io.InputStreamReader;
 //import java.net.ServerSocket;
 import java.net.Socket;
@@ -28,6 +29,9 @@ public class ClientRequestHandler implements Runnable {
 		// Thread local variables
 		RequestObject currentRequest = null;
 		
+		// test file for transmission
+		String testFile = new String("M:/w2k/My Pictures/norgate.gif");
+		
 		while (!done) {
 			System.out.printf("[INFO] Waiting for data from client \n");
 			currentRequest = getRequest(clientSocket);
@@ -39,7 +43,9 @@ public class ClientRequestHandler implements Runnable {
 					done = true;
 				
 				default:
-					System.out.printf("[INFO] Request ''%s'' not recognised \n", currentRequest.id);
+					System.out.printf("[INFO] Request '%s' not recognised \n", currentRequest.id);
+					// notify client that request was not recognised
+					sendResponse(clientSocket, "Request not recognised", null);
 					break;
 			}
 		}
@@ -72,4 +78,21 @@ public class ClientRequestHandler implements Runnable {
 		}
 		return null;
 	}
+	
+	
+	
+	// method for sending a response to the client
+	private void sendResponse(Socket socket, String id, Object param){
+		// instantiate request object
+		RequestObject thisRequest = new RequestObject(id, param);
+		try {
+			ObjectOutputStream outputStream = new ObjectOutputStream(socket.getOutputStream());
+			outputStream.writeObject(thisRequest);
+			outputStream.flush();
+		} catch(IOException e) {
+			System.out.printf("[ERROR] Failed to send respond (IOException)");
+			e.printStackTrace();
+		} 
+	}
+	
 }
--- server/StammtischServer.java
+++ server/StammtischServer.java
@@ -1,21 +1,38 @@
+/*
+* (C) Stammtisch
+* First version created by: Alexander Cramb (ac1362)
+* Date of first version: 28/01/2016
+* 
+* Last version by: Alexander Cramb (ac1362)
+* Date of last update: 
+* Version number: 0.1
+* 
+* Commit date: 
+* Description: 
+* Load bearing server for stammtisch application
+*/
+
+
 package server;
 
 
-import java.io.BufferedReader;
+//import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+//import java.io.InputStreamReader;
 import java.net.ServerSocket;
-import java.net.Socket;
+//import java.net.Socket;
+import java.util.Scanner;
 
 
 public class StammtischServer {
 	
 	// Socket stuff
-	public static int defaultPort = 26656; //Port to run server on
+	public static int defaultPort = 26656; // Port to run server on
 	public ServerSocket sSocket;
 	
 	
 	
+	// server constructor. Launches connectio9n listener thread
 	public StammtischServer(int port) {
 		// Indicate that server is starting
 		System.out.printf("[INFO] Starting server on port %d\n", port);
@@ -28,7 +45,7 @@ public class StammtischServer {
 	
 	
 	// Connection listener thread
-	// waits for incoming socket connection
+	// waits for incoming connection
 	Thread connectionListener = new Thread("Stammtish connection listener") {
 		public void run() {
 			try {
@@ -53,10 +70,40 @@ public class StammtischServer {
 	
 	
 	// Main method for launching server
-	public static void main(String[] args){
+	public static void main(String[] args) {
+		// do input argument passing here
+		
+		// start the stammtisch server
 		new StammtischServer(defaultPort);
+		
+		// "scanner" object for reading from command line (javawtf?)
+		Scanner commandScanner = new Scanner(System.in);	
+		String command = new String();	// command typed on the command line
+		boolean done = false;			// signal variable for ending server operation
+		
+		// command handling loop
+		while(!done) {
+			// wait for command input
+			command = commandScanner.next();
+			
+			// handle command input
+			switch(command) {
+				// exit command 
+				case "stop":
+					System.out.printf("[INFO] Stopping stammtisch server.\n");
+					done = true;
+					break;
+					
+				// unrecognised command
+				default:
+					System.out.printf("[ERROR] Unrecognised command '%s'.\n", command);
+					break;
+			}
+		}
+		// close command scanner
+		commandScanner.close();
+		
+		// shut down server & all worker threads
+		System.exit(0);
 	}
-
 }
-
-
