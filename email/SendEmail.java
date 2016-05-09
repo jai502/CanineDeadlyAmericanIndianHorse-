cc838850403adec89f0233ad55958de4250429fc
jonathanbones <jb1237@york.ac.uk>
2016-05-09 22:01:23 +0100

    HTML email content with automatic configuration of gmail account

--- email/SendEmail.java
+++ email/SendEmail.java
@@ -1,3 +1,16 @@
+/**
+* (C) Stammtisch
+* First version created by: D.Dreyfus
+* Date of first version: 06.05.2016
+* 
+* Last version by: J.Bones
+* Date of last update: 09.05.2016
+* Version number: 1.2
+* 
+* Commit date: 09.05.2016
+* Description: Module to allow for sending of Emails upon user registration
+ */
+
 package email;
 
 import java.util.Properties;
@@ -12,46 +25,53 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import javax.swing.JOptionPane;
 
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
-
 public class SendEmail 
 {
 
     public static void send(String to, String sub,String msg, final String user_email, final String password) 
     {
-        Properties props = new Properties();
+        Properties properties = new Properties();
+        String host = "smtp.gmail.com";
+        int port = 587;
 
-        props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.port", "587");	
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
+      //Set up the system for sending email
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.smtp.host", host);
+        properties.put("mail.smtp.socketFactory.port", port);
+        properties.put("mail.smtp.sender.address", user_email);
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.debug", "true");
+        properties.put("mail.smtp.EnableSSL.enable","true");
+
+        //Code here was taken from
+        //Reference http://stackoverflow.com/questions/386083/must-issue-a-starttls-command-first-sending-email-with-java-and-google-apps
+        properties.setProperty("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        properties.setProperty("mail.smtp.socketFactory.fallback", "false");
+        properties.setProperty("mail.smtp.port", "465");
+        properties.setProperty("mail.smtp.socketFactory.port", "465");
         
-        Session session = Session.getDefaultInstance(props,new Authenticator() 
-        {
-            @Override
-            protected PasswordAuthentication getPasswordAuthentication() 
-            {
-                return new PasswordAuthentication(user_email, password);
-            }
-        });
+        Authenticator authenticator = new Authenticator() {
+          public PasswordAuthentication getPasswordAuthentication(){
+              return new PasswordAuthentication(user_email, password);
+          }
+      };
+
+      //Get the default session stuff
+      Session session = Session.getDefaultInstance(properties, authenticator);
+      Message message = new MimeMessage(session);
 
         try 
         {
-            Message message = new MimeMessage(session);
-            
             message.setFrom(new InternetAddress(user_email));
             message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
-            message.setSubject(sub);
-            message.setText(msg);
+            message.setSubject("Welcome to Stammtisch!");
             
-            //message.setContent(msg, "text/html");
-
+            //Create the email content using HTML
+            message.setContent(msg, "text/html");
+            
+            //Send the message
             Transport.send(message);
-            
-            Alert alert = new Alert(AlertType.CONFIRMATION, "Email sent!");
-            alert.show();
-            
+            System.out.println("Email Sent!");
         } catch (MessagingException e) 
         {
             JOptionPane.showMessageDialog(null,"Something happened!");
